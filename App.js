669264d6b3dc8533810f2a0c3ca0bb6502039b36
Nikhil <nikhil4.deswal@gmail.com>
2022-10-10 23:07:06 -0700

    directory error fixed

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
-import Navigation from '../MoviesApp/src/navigation/Navigation';
+import Navigation from '../wmdd-4998-movie-app-nikhil/src/navigation/Navigation';
 
 export default function App() {
   return (
     <NativeBaseProvider>
-     <Navigation />
-     <StatusBar barStyle="light-content" />
+      <Navigation />
+      <StatusBar barStyle="light-content" />
     </NativeBaseProvider>
   );
 }
